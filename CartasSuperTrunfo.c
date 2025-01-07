c7b95d84f2bddbfbed3bc9a0eb1905faf7a2dc1c
mrmaciel <49823825+mrmaciel@users.noreply.github.com>
2025-01-07 03:03:30 +0000

    Primeiros códigos do desafio Aventureiro.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,24 +8,29 @@
 int main() {
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+    char   cEstado;        //Estado
     int    iCodCid;        //Código da Cidade
     char   sNomeCid[50];   //Nome da Cidade
     int    iPopulacao;     //População
     float  fArea;          //Área
+    // float  fDensPop;       //Densidade Populacional
     double dPib;           //PIB
+    // double dPibPerCapta;   //PIB per Capta    
     int    iNumPtTur;      //Número de pontos turísticos 
     
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    printf("Informe o Código da Cidade:\n");
+    // Solicite ao usuário que insira as informações de cada cidade, como o estado, código da carta, nome, população, área, etc.
+    printf("Informe o Estado;\n");
+    scanf("%c", &cEstado);
+    printf("Informe o Código da Carta:\n");
     scanf("%d", &iCodCid);
     printf("Informe o Nome da Cidade:\n");
     scanf("\n%[^\n]",sNomeCid);
     printf("Informe a População da Cidade:\n");
     scanf("%d", &iPopulacao);
     printf("Informe a Área da Cidade:\n");
-    scanf("%f", &fArea);
+    scanf("%f", &fArea);    
     printf("Informe o Pib da Cidade:\n");
     scanf("%lf", &dPib);
     printf("Informe o Número de Pontos Turísticos da Cidade:\n");
@@ -35,11 +40,14 @@ int main() {
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
     printf("\n");
-    printf("Código da Cidade: %d\n", iCodCid);
+    printf("Estado: %c\n", cEstado);    
+    printf("Código da Carta: %d\n", iCodCid);
     printf("Cidade: %s\n", sNomeCid);
     printf("População: %d\n", iPopulacao);
-    printf("Área: %.2f\n", fArea);
-    printf("Pib: %.2f\n", dPib);
+    printf("Área: %.2f km²\n", fArea);
+    printf("Densidade Populacional:  pessoas\km²\n");
+    printf("PIB: %.2f reais\n", dPib);
+    printf("PIB per capta: reais\n");
     printf("Número de Pontos Turísticos: %d\n", iNumPtTur);
     printf("\n");
 
