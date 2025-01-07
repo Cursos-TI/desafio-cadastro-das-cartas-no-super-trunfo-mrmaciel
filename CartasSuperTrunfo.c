f45fa077d15208a4c2061e9ddd05ec5afb97076e
mrmaciel <49823825+mrmaciel@users.noreply.github.com>
2025-01-07 11:36:57 +0000

    Ajustes gerais.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -12,16 +12,14 @@ int main() {
     int    iCodCid;        //Código da Cidade
     char   sNomeCid[50];   //Nome da Cidade
     int    iPopulacao;     //População
-    float  fArea;          //Área
-    // float  fDensPop;       //Densidade Populacional
-    double dPib;           //PIB
-    // double dPibPerCapta;   //PIB per Capta    
+    float  fArea;          //Área    
+    double dPib;           //PIB    
     int    iNumPtTur;      //Número de pontos turísticos 
     
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o estado, código da carta, nome, população, área, etc.
-    printf("Informe o Estado;\n");
+    // Solicite ao usuário que insira as informações de cada cidade, como o estado, código da cidade, nome, população, área, etc.
+    printf("Informe o Estado:\n");
     scanf("%c", &cEstado);
     printf("Informe o Código da Carta:\n");
     scanf("%d", &iCodCid);
@@ -41,13 +39,13 @@ int main() {
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
     printf("\n");
     printf("Estado: %c\n", cEstado);    
-    printf("Código da Carta: %d\n", iCodCid);
+    printf("Código da Carta: %c0%d\n", cEstado, iCodCid);
     printf("Cidade: %s\n", sNomeCid);
     printf("População: %d\n", iPopulacao);
     printf("Área: %.2f km²\n", fArea);
-    printf("Densidade Populacional:  pessoas\km²\n");
+    printf("Densidade Populacional: %.2f pessoas/km²\n", iPopulacao/fArea);
     printf("PIB: %.2f reais\n", dPib);
-    printf("PIB per capta: reais\n");
+    printf("PIB per capta: %.2lf reais\n", dPib/iPopulacao);
     printf("Número de Pontos Turísticos: %d\n", iNumPtTur);
     printf("\n");
 
